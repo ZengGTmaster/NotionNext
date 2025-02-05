5f8e76337e68b07dd957332991ce7d8edd703e22
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2025-02-05 18:20:40 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
   HEO_NOTICE_BAR: [
     { title: 'Hi，我是B站up-无心月空, 2025年新的启航', url: 'www.mskyer.com' },
     { title: '平凡吉他（初级篇）正在录制更新中', url: 'https://www.bilibili.com/video/BV1MVsseBEwy/?spm_id_from=333.999.0.0' }
-    { title: '在正安学做琴，教学没有放下，事多更新比较慢，有购书需求请直接加微信18989133114', url: '' }
+    { title: '在正安学做琴，教学没有放下，事多更新比较慢，有购书需求请直接加微信18989133114',  }
   ],
 
   // 英雄区左右侧组件颠倒位置
