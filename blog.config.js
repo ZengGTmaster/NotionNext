4789e617a3ab19046e440f6aefc506857f8927b0
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2025-02-05 18:09:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '无心月空', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || ''专注吉他教学，初学者救星，难关终结者,社交恐怖分子', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '专注吉他教学，初学者救星，难关终结者,社交恐怖分子', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'www.mskyer.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '无心月空, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
