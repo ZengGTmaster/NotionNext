9e0ce68dac2acb97059ab473c2ae8b999a4be807
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2025-02-05 17:53:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    'Hi，我是B站up-无心月空, 2025年新的启航，我在贵州正安学做琴，教学也是没有放下的，事情太多，更新会很慢,欢迎来到我的博客🎉，有购书需求请直接加微信18989133114',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
