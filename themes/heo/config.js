23a9a9028cdd1c08f6043ad44916ae4ed4149c50
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2025-07-14 23:27:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客，我是B站无心月空', url: 'https://space.bilibili.com/156820044?spm_id_from=333.1007.0.0' },
+    { title: '我正在贵州学习做吉他，教学暂时放下，专心学习', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
