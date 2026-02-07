04acfe8c62cc4b1ce929a84202b4026b15a59600
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2026-02-07 23:44:24 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -96,7 +96,7 @@ const CONFIG = {
     '数千位站长选择用NotionNext搭建他们的网站,通过帮助手册、交流社群以及技术咨询，大家成功上线了自己的网站',
 
   // 用户测评处的跳转按钮
-  PROXIO_TESTIMONIALS_BUTTON_URL: '/about',0
+  PROXIO_TESTIMONIALS_BUTTON_URL: '/about',
   PROXIO_TESTIMONIALS_BUTTON_TEXT: '联系我',
 
   // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
