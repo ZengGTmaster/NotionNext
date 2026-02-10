b6373eb46b3617f0846b46e47f5ded86ff2af397
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2026-02-10 16:23:47 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -13,24 +13,24 @@ const CONFIG = {
   PROXIO_HERO_BUTTON_1_TEXT: 'B站主页', // 英雄区按钮
   PROXIO_HERO_BUTTON_1_URL:
     'https://space.bilibili.com/156820044?spm_id_from=333.337.0.0', // 英雄区按钮
-  PROXIO_HERO_BUTTON_1_ICON: '', // 英雄区按钮1的图标，不需要则留空  #/images/starter/github-mark.svg
+  PROXIO_HERO_BUTTON_1_ICON: '', // 英雄区按钮1的图标
   
   PROXIO_HERO_BUTTON_2_TEXT: '', // 英雄区按钮
   PROXIO_HERO_BUTTON_2_URL: '', // 英雄区按钮
-  PROXIO_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空  #/images/starter/github-mark.svg
+  PROXIO_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标
 
-  // 英雄区配图，如需隐藏，改为空值即可 ''
-  PROXIO_HERO_BANNER_IMAGE: '', // hero区背景，默认是获取Notion背景，如需另外配置图片可以填写在这里
-  PROXIO_HERO_BANNER_IFRAME_URL: '', // hero背景区内嵌背景网页 ，可以配置一个网页地址，例如动画网页https://my.spline.design/untitled-b0c6e886227646c34afc82cdc6de4ca2/
+  // 英雄区配图
+  PROXIO_HERO_BANNER_IMAGE: '', 
+  PROXIO_HERO_BANNER_IFRAME_URL: '', 
 
   // 文章区块
   PROXIO_BLOG_ENABLE: true, // 首页博文区块开关
   PROXIO_BLOG_TITLE: '寄蜉蝣于天地，渺沧海之一粟。哀吾生之须臾，羡长江之无穷',
-  PROXIO_BLOG_COUNT: 6, // 首页博文区块展示前4篇文章
+  PROXIO_BLOG_COUNT: 6, // 首页博文区块展示前6篇文章
   PROXIO_BLOG_TEXT_1: '',
 
-  // 区块默认内容显示文章的summary文本，但也支持用自定义图片或logo德国替换掉占位显示内容
-  PROXIO_BLOG_PLACEHOLDER_IMG_URL_1: '', // 填写要替换成的图片，支持图床或直接上传到项目中，示例  /images/feature-1.webp
+  // 区块默认内容显示
+  PROXIO_BLOG_PLACEHOLDER_IMG_URL_1: '', 
   PROXIO_BLOG_PLACEHOLDER_IMG_URL_2: '',
   PROXIO_BLOG_PLACEHOLDER_IMG_URL_3: '',
   PROXIO_BLOG_PLACEHOLDER_IMG_URL_4: '',
@@ -44,8 +44,8 @@ const CONFIG = {
   PROXIO_FEATURE_TEXT_2: '丰富的教学经验，扎实的基本功，专业的吉他技能，拥有十足的耐心',
 
   // 特性1
-  PROXIO_FEATURE_1_ICON_CLASS: 'fa-solid fa-stopwatch', // fas图标
-  PROXIO_FEATURE_1_ICON_IMG_URL: '', // 图片图标选填，默认是fas图标，如果需要图片图标可以填写图片地址，示例/avatar.png
+  PROXIO_FEATURE_1_ICON_CLASS: 'fa-solid fa-stopwatch', 
+  PROXIO_FEATURE_1_ICON_IMG_URL: '', 
   PROXIO_FEATURE_1_TITLE_1: '时间成本',
   PROXIO_FEATURE_1_TEXT_1:
     '沟通高效、即时行动，不在无意义的事情上浪费时光，节约你的时间。',
@@ -61,128 +61,58 @@ const CONFIG = {
   PROXIO_FEATURE_3_TEXT_1:
     '了解学生，在适当的时机给予一些安全、品德、生活经验等教育，不只是卖课赚钱',
 
-  PROXIO_FEATURE_BUTTON_TEXT: '了解更多（暂时空）', // 按钮文字
-  PROXIO_FEATURE_BUTTON_URL: 'https://www.mskyer.com', // 按钮跳转
+  // 🍎 修改点：点击“了解更多”直接跳转到博文分类页
+  PROXIO_FEATURE_BUTTON_TEXT: '查看更多博文', // 按钮文字
+  PROXIO_FEATURE_BUTTON_URL: '/category/博文', // 修正跳转地址
 
   // 首页生涯区块
-  PROXIO_CAREER_ENABLE: false, // 区块开关
+  PROXIO_CAREER_ENABLE: false, 
   PROXIO_CAREER_TITLE: '生涯',
   PROXIO_CAREER_TEXT: '以下是我的职业生涯',
 
-  // 生涯内容卡牌 ，title是标题 ，bio是备注，text是详情
   PROXIO_CAREERS: [
     {
       title: 'Freelance Architect',
       bio: '2016-2020',
-      text: 'As a freelance architect, I worked on a range of residential and commercial projects, balancing form and function. I collaborated with clients and contractors to transform concepts into reality, ensuring each design was both aesthetically pleasing and structurally sound.'
-    },
-    {
-      title: 'Product Designer at Spotify',
-      bio: '2020-2022',
-      text: 'At Spotify, I helped shape innovative features for millions of users globally. My focus was on creating seamless music discovery experiences, enhancing user interfaces, and optimizing cross-platform navigation, which led to an improved product flow and increased user satisfaction.'
-    },
-    {
-      title: 'Freelance Product Designer',
-      bio: '2022-Now',
-      text: 'Now I design user-centric products that align with client visions. As a freelance product designer, I collaborate with startups and established companies, crafting solutions that elevate user experiences and increase engagement across both digital and physical interfaces.'
+      text: 'As a freelance architect...'
     }
   ],
 
   // 首页用户测评区块
-  PROXIO_TESTIMONIALS_ENABLE: false, // 测评区块开关
+  PROXIO_TESTIMONIALS_ENABLE: false, 
   PROXIO_TESTIMONIALS_TITLE: '用户反馈',
   PROXIO_TESTIMONIALS_TEXT_1: '我们的用户怎么说',
-  PROXIO_TESTIMONIALS_TEXT_2:
-    '数千位站长选择用NotionNext搭建他们的网站,通过帮助手册、交流社群以及技术咨询，大家成功上线了自己的网站',
+  PROXIO_TESTIMONIALS_TEXT_2: '数千位站长选择用NotionNext搭建他们的网站',
 
-  // 用户测评处的跳转按钮
   PROXIO_TESTIMONIALS_BUTTON_URL: '/about',
   PROXIO_TESTIMONIALS_BUTTON_TEXT: '联系我',
 
-  // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
   PROXIO_TESTIMONIALS_ITEMS: [
     {
-      PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '感谢大佬的方法。之前尝试过Super、Potion等国外的第三方平台，实现效果一般，个性化程度远不如这个方法，已经用起来了！ ',
-      PROXIO_TESTIMONIALS_ITEM_AVATAR:
-        'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F22de3fcb-d90d-4271-bc01-f815f476122b%2F4FE0A0C0-E487-4C74-BF8E-6F01A27461B8-14186-000008094BC289A6.jpg?table=collection&id=a320a2cc-6ebe-4a8d-95cc-ea94e63bced9&width=200',
+      PROXIO_TESTIMONIALS_ITEM_TEXT: '感谢大佬的方法...',
+      PROXIO_TESTIMONIALS_ITEM_AVATAR: 'https://www.notion.so/image/...",
       PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Ryan_G',
       PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Ryan`Log 站长',
       PROXIO_TESTIMONIALS_ITEM_URL: 'https://blog.gaoran.xyz/'
-    },
-    {
-      PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '很喜欢这个主题，本代码小白用三天台风假期搭建出来了，还根据大佬的教程弄了自定义域名，十分感谢，已请喝咖啡~',
-      PROXIO_TESTIMONIALS_ITEM_AVATAR:
-        'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d33d169-f932-41ff-ac6b-88a923c08e02%2F%25E5%25A4%25B4%25E5%2583%258F.jfif?table=collection&id=7787658d-d5c0-4f34-8e32-60c523dfaba3&width=400',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'Asenkits',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '阿森的百宝袋 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://asenkits.top/'
-    },
-    {
-      PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '呜呜呜，经过一个下午的努力，终于把博客部署好啦，非常感谢Tangly1024大佬的框架和教程，这是我有生之年用过的最好用的博客框架┭┮﹏┭┮。从今之后，我就可以在自己的博客里bb啦，( •̀ ω •́ )y ',
-      PROXIO_TESTIMONIALS_ITEM_AVATAR: '/avatar.png',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'DWIND',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: '且听风吟 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.dwind.top/'
-    },
-    {
-      PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '感谢提供这么好的项目哈哈 之前一直不知道怎么部署(别的项目好难好复杂)这个相对非常简单 新手非常友好哦',
-      PROXIO_TESTIMONIALS_ITEM_AVATAR:
-        'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd52f6766-3e32-4c3d-8529-46e1f214360f%2Ffavicon.svg?table=collection&id=7d76aad5-a2c4-4d9a-887c-c7913fae4eed&width=400',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: '迪升disheng ',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'AI资源分享 Blog',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://blog.disheng.org/'
-    },
-    {
-      PROXIO_TESTIMONIALS_ITEM_TEXT:
-        '灰常感谢大佬的博客项目，能将博客和notion结合起来，这一直是我挺期待的博客模式。',
-      PROXIO_TESTIMONIALS_ITEM_AVATAR: '/avatar.png',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'AnJhon',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'Anjhon`s Blog 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.anjhon.top'
-    },
-    {
-      PROXIO_TESTIMONIALS_ITEM_TEXT: '用好久了，太感谢了',
-      PROXIO_TESTIMONIALS_ITEM_AVATAR: '/avatar.png',
-      PROXIO_TESTIMONIALS_ITEM_NICKNAME: 'LUCEN',
-      PROXIO_TESTIMONIALS_ITEM_DESCRIPTION: 'LUCEN考验辅导 站长',
-      PROXIO_TESTIMONIALS_ITEM_URL: 'https://www.lucenczz.top/'
     }
   ],
 
-  //   FAQ 常见问题模块
-  PROXIO_FAQ_ENABLE: false, // 常见问题模块开关
+  // FAQ 常见问题模块
+  PROXIO_FAQ_ENABLE: false, 
   PROXIO_FAQ_TITLE: '常见问题解答',
-  PROXIO_FAQ_TEXT_1: '有任何问题吗？请看这里',
-  PROXIO_FAQ_TEXT_2: '我们收集了常见的用户疑问',
   PROXIO_FAQS: [
     {
       q: 'NotionNext有帮助文档吗？',
-      a: 'NotionNext提供了<a href="https://docs.tangly1024.com/about" className="underline">帮助文档</a>，操作<a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">演示视频</a>，以及<a href="https://docs.tangly1024.com/article/chat-community" className="underline">交流社群</a>来协助您完成网站的搭建部署'
-    },
-    {
-      q: '部署后要如何编写文章？',
-      a: '您可以在Notion中之间添加或修改类型为Post的页面，内容将被实时同步在站点中，详情参考<a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《帮助文档》</a>'
-    },
-    {
-      q: '站点部署失败，更新失败？',
-      a: '通常是配置修改错误导致，请检查配置或者重试操作步骤，或者通过Vercel后台的Deployments中找到错误日志，并向网友求助'
-    },
-    {
-      q: '文章没有实时同步？',
-      a: '先检查Notion_Page_ID是否正确配置，其次由于博客的每个页面都有独立缓存，刷新网页后即可解决'
+      a: 'NotionNext提供了帮助文档...'
     }
   ],
 
   // 关于作者区块
-  PROXIO_ABOUT_ENABLE: true, // 关于作者区块区块开关
+  PROXIO_ABOUT_ENABLE: true, 
   PROXIO_ABOUT_TITLE: '关于作者',
   PROXIO_ABOUT_TEXT_1: '我是深耕好多人看不上，懒得干，来钱慢的教学领域',
   PROXIO_ABOUT_TEXT_2:
-    '太多人对社会上的琴行、培训机构不信任，是因为教吉他门槛低，不认真做、没水平的人太多。也是为什么在学校里面挤破头都想把孩子送到好老师的班里。经验丰富、实践多年、有创作......而我就是你需要的人。',
+    '太多人对社会上的琴行、培训机构不信任，是因为教吉他门槛低，不认真做、没水平的人太多。而我就是你需要的人。',
   PROXIO_ABOUT_PHOTO_URL: '/avatar.png',
   PROXIO_ABOUT_KEY_1: '小学公办教师经历',
   PROXIO_ABOUT_VAL_1: '15年',
@@ -197,7 +127,7 @@ const CONFIG = {
   PROXIO_ABOUT_BUTTON_TEXT: '关于我',
 
   // 横向滚动文字
-  PROXIO_BRANDS_ENABLE: false, // 滚动文字
+  PROXIO_BRANDS_ENABLE: false, 
   PROXIO_BRANDS: [
     'Beyond曲集',
     '精通指板',
@@ -208,25 +138,21 @@ const CONFIG = {
     '电子谱集',
   ],
 
-  PROXIO_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+  PROXIO_FOOTER_SLOGAN: '吉他教学不仅是技艺的传授，更是音乐灵魂的共鸣。',
 
-  // 页脚三列菜单组
-  // 页脚菜单
-
-  // 404页面的提示语
+  // 404页面
   PROXIO_404_TITLE: '我们似乎找不到您要找的页面。',
-  PROXIO_404_TEXT: '抱歉！您要查找的页面不存在。可能已经移动或删除。',
+  PROXIO_404_TEXT: '抱歉！您要查找的页面不存在。',
   PROXIO_404_BACK: '回到主页',
 
-  // 页面底部的行动呼吁模块
+  // 行动呼吁模块
   PROXIO_CTA_ENABLE: false,
   PROXIO_CTA_TITLE: '与我建立联系',
   PROXIO_CTA_TITLE_2: '微信：18989133114 坐标：成都',
-  PROXIO_CTA_DESCRIPTION:
-    '提供一对一线下教学，或者一对一线上教学，针对每个人的情况制定、调整学习课程',
+  PROXIO_CTA_DESCRIPTION: '提供一对一线下教学，或者一对一线上教学。',
 
-  PROXIO_POST_REDIRECT_ENABLE: false, // 默認開啟重定向
-  PROXIO_POST_REDIRECT_URL: 'https://blog.tangly1024.com', // 重定向域名
-  PROXIO_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_PROXIO_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
+  PROXIO_POST_REDIRECT_ENABLE: false, 
+  PROXIO_POST_REDIRECT_URL: 'https://www.mskyer.com', 
+  PROXIO_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_PROXIO_NEWSLETTER || false 
 }
 export default CONFIG
