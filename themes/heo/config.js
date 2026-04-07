ee0bbdaedf966730eb95f7d601f420a735918c86
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2026-04-07 22:55:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '点击跳转到我的B站空间，我的主要根据地', url: 'https://space.bilibili.com/156820044?spm_id_from=333.1007.0.0' },
+    { title: '曾老师教学能力可以到我的B站看，或者我的博客，战绩可查', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,18 +18,18 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '琴棋书画',
+  HEO_HERO_TITLE_2: '一同修身养性，获得学到技能的快乐',
+  HEO_HERO_TITLE_3: '',
+  HEO_HERO_TITLE_4: '',
+  HEO_HERO_TITLE_5: '大音希声，大象无形。',
+  HEO_HERO_TITLE_LINK: '',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_2: { title: '日常随发', url: '/tag/日常' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
